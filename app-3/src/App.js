3e23610803401ebf9bb895720509892e92c821aa
BrianDeSplinter <bdesplinter34@gmail.com>
2020-05-13 15:50:15 -0600

    Solution for App3

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component{
+  constructor(){
+    super()
+    this.state = {
+      stuff: ['Backpack', 'Tent', 'Water', 'Food', 'Sleeping Bag', 'Pillow', 'Flashlight', 'Firewood', 'Tp', 'Sunscreen', 'Bug Spray'],
+      input: ''
+    }
+  }
 
+  userInput(val){
+    this.setState({
+      input: val
+    })
+  }
+
+
+  render(){
+    let stuffArr = this.state.stuff.filter((e,i) => {
+      return e.includes(this.state.input)
+    })
+    .map((e,i) => {
+        return <h2 key = {i}>{e}</h2>
+      })
+    
+    return (
+    <div className = 'App'>
+      <input onChange = {(e) => this.userInput(e.target.value)}/>
+      <div>{stuffArr}</div>
+    </div>
+    )
+
+  }
+}
 export default App;
