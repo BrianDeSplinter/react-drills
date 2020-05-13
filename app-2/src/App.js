210e41bfd2bf48b8048807258534da9a560b7d79
BrianDeSplinter <bdesplinter34@gmail.com>
2020-05-13 14:57:09 -0600

    Solution for App2

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(){
+    super()
+    this.state = {
+      arr: ['Quinn', 'Ani', 'Obi', 'Pheobe', 'Max', 'Stasheff']
+    }
+  }
+
+  // h4Array(arr){
+  //   let arrH = []
+  //   for(let i=0; i<arr.length; i++){
+      
+  //   }
+  // }
+  render(){
+    let arrH = this.state.arr.map((e,i) =>{
+      return <h2 key = {i}>{e}</h2>
+      })
+    
+    return <div className = 'App'>{arrH}</div>
+  }
 }
+
 
 export default App;
